b3386a2fcab3f895da8da87fa399421dc2d56c53
Sahil Sharma <vlsharma713@gmail.com>
2025-06-19 02:07:06 +0530

    Add animated gradient

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,16 +1,20 @@
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
+import AnimatedGradient from "./ui/animated-gradient";
 
 export default function Hero() {
   return (
-		<section className="container flex min-h-[calc(100vh-3.5rem)] max-w-screen-2xl flex-col items-center justify-center space-y-8 py-24 text-center md:py-32">
+		<section className="container flex min-h-[calc(100dvh-3.5rem)] max-w-screen-2xl flex-col items-center justify-center space-y-8 py-10 text-center md:py-32">
+      <div className="absolute h-[80dvh] px-10 -z-10 w-full">
+        <AnimatedGradient className="rounded-3xl"/>
+      </div>
 			<div className="space-y-4">
 				<h1 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-3xl md:text-5xl lg:text-6xl">
 					Master AI & Machine Learning:
 					<br />
 					From Zero to Industry Expert
 				</h1>
-				<p className="mx-auto max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+				<p className="mx-auto max-w-[42rem] leading-normal sm:text-xl sm:leading-8">
 					The complete AI, ML & Python - Learn from NVIDIA & IBM Mentors in this
 					55 - Hour Program
 				</p>
