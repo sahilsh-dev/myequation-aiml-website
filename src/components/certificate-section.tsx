729f11c96cff2a0936ec1957dd57f8069b44eb9d
Akash Mishra <104095792+Akash5746@users.noreply.github.com>
2025-06-22 18:31:53 +0000

    add-enrollment, certification-margin-bottom, add-image,

--- src/components/certificate-section.tsx
+++ src/components/certificate-section.tsx
@@ -6,7 +6,7 @@ import Certificate from "./ui/certificate";
 
 export default function CertificateSection() {
   return (
-    <section className="container w-full max-w-[1200px] mx-auto pt-20 mb-16 sm:mb-32">
+    <section className="container w-full max-w-[1200px] mx-auto pt-20  ">
       {/* Section Title */}
       <div className="text-center px-4">
         <h2 className="font-bold text-2xl md:text-5xl leading-tight text-white m-0">
