f98dbc9088e9265b7c2d9575e4ab8a57a12aafae
Sahil Sharma <vlsharma713@gmail.com>
2025-06-22 20:27:47 +0530

    Update certificate section

--- src/components/certificate-section.tsx
+++ src/components/certificate-section.tsx
@@ -2,21 +2,23 @@ import Image from "next/image";
 import nvidiaLogo from "@/assets/nvidia-logo.png";
 import ibmLogo from "@/assets/ibm-logo-white.png";
 import { Separator } from "./ui/separator";
+import Certificate from "./ui/certificate";
 
 export default function CertificateSection() {
   return (
     <section className="container w-full max-w-[1200px] mx-auto pt-20 mb-16 sm:mb-32">
       {/* Section Title */}
-      <div className="text-center mb-12 px-4">
+      <div className="text-center px-4">
         <h2 className="font-bold text-2xl md:text-5xl leading-tight text-white m-0">
           Certificates awarded in partnership with
         </h2>
-        <div className="flex items-center justify-center my-5">
+        <div className="flex items-center justify-center mt-5">
           <Image src={nvidiaLogo} alt="NVIDIA Logo" className="w-24" />
           <Separator orientation="vertical" className="mx-7 h-16" />
           <Image src={ibmLogo} alt="IBM Logo" className="w-32" />
         </div>
       </div>
+      <Certificate />
     </section>
   );
 }
---
+++ src/components/ui/certificate.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import certificateImg from "@/assets/certificate.jpg";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+
+export default function Certificate() {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[40dvw] h-auto rounded-xl p-1 md:p-6 border">
+        <CardItem translateZ="100" className="w-full p-4">
+          <Image
+            src={certificateImg}
+            className="object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="certificate"
+          />
+        </CardItem>
+      </CardBody>
+    </CardContainer>
+  );
+}
