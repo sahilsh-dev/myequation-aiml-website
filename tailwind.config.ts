b3ad7814b653c6e5a0d10a35e159eef5cba6abe4
Sahil Sharma <vlsharma713@gmail.com>
2025-06-23 23:34:42 +0530

    Fix scroll

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,10 +69,14 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "scroll": {
+          to: { transform: "translateX(-50%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "scroll": "scroll var(--animation-duration,40s) linear infinite var(--animation-direction,forwards)",
       },
     },
   },
