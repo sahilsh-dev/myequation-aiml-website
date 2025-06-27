d5d626fbe4009f72de75a65621d6bdd95c9e32bd
Sahil Sharma <vlsharma713@gmail.com>
2025-06-28 00:45:52 +0530

    Update accordion to open single element

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -18,19 +18,16 @@ export default function Accordion({
   items: DropdownItem[];
   className?: string;
 }) {
-  const [expandedItems, setExpandedItems] = useState<number[]>([]);
+  const [expandedItem, setExpandedItem] = useState<number>();
 
   const toggleItem = (id: number) => {
-    setExpandedItems((prev) =>
-      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
-    );
+    setExpandedItem(id);
   };
 
   return (
     <div className={cn("space-y-4", className)}>
       {items.map((item) => (
         <ScrollReveal direction="up" key={item.id} delay={0.1 * item.id}>
-
           <div
             key={item.id}
             className="border border-gray-600 rounded-lg overflow-hidden"
@@ -39,8 +36,10 @@ export default function Accordion({
               onClick={() => toggleItem(item.id)}
               className="w-full px-6 py-4 text-left  hover:bg-gray-900 transition-colors duration-300 flex items-center justify-between"
             >
-              <span className="text-white font-medium text-sm md:text-lg">{item.title}</span>
-              {expandedItems.includes(item.id) ? (
+              <span className="text-white font-medium text-sm md:text-lg">
+                {item.title}
+              </span>
+              {expandedItem === item.id ? (
                 <ChevronUp className="w-5 h-5 text-yellow-400" />
               ) : (
                 <ChevronDown className="w-5 h-5 text-yellow-400" />
@@ -50,17 +49,18 @@ export default function Accordion({
             {/* Animated content */}
             <div
               className={cn(
-                "transition-all duration-300  overflow-hidden px-6 border-t border-gray-600",
-                expandedItems.includes(item.id)
+                "transition-all duration-300 overflow-hidden px-6 border-t border-gray-600",
+                expandedItem === item.id
                   ? "max-h-96 opacity-100 py-4"
                   : "max-h-0 opacity-0 py-0"
               )}
               style={{
-                // fallback for maxHeight if content is larger
                 transitionProperty: "max-height, opacity, padding",
               }}
             >
-              <p className="text-gray-300 leading-relaxed text-xs md:text-sm">{item.description}</p>
+              <p className="text-gray-300 leading-relaxed text-xs md:text-sm">
+                {item.description}
+              </p>
             </div>
           </div>
         </ScrollReveal>
